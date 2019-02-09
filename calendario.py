9b0bb445a79f206934b4cf6ba97b4dc57b4a24c3
Alejandra <aleizaguirrehernandez@gmail.com>
2019-02-09 23:54:11 +0100

    Esto esta fatal

--- calendario.py
+++ calendario.py
@@ -12,6 +12,7 @@ currentMonth = datetime.now().month
 currentYear = datetime.now().year
 """
 
+
 class calendarButton(ttk.Frame):
     def __init__(self, parent, **args):
         ttk.Frame.__init__(self, parent, height=30, width=51)
@@ -20,14 +21,23 @@ class calendarButton(ttk.Frame):
         self.__btn = ttk.Button(self, text=args["text"])
         self.__btn.pack(fill=BOTH, expand=1)
 
+
 class mesDisplay(ttk.Frame):
-    __value = datetime.date.month
 
+    dictMes = {"1":"Enero","2":"Febrero","3":"Marzo","4":"Abril","5":"Mayo","6":"Junio","7":"Julio","8":"Agosto","9":"Septiembre","10":"Octubre","11":"Noviembre","12":"Diciembre"}
+    mesActual = dictMes[str(datetime.now().month)]
+    añoActual = datetime.now().year
 
-    def __init__(self,parent,**args):
-        
+    __value = mesActual, añoActual
+
+    def __init__(self, parent, **args):
+
+        ttk.Frame.__init__(self,parent)
         self.__labelframe = ttk.LabelFrame(self, text=self.__value)
         self.__labelframe.pack(fill=BOTH)
+
+        s = ttk.Style()
+        s.configure("my.TLabel",font=("Arial",24))
 
         left = ttk.Label(self.__labelframe, text="Aqui van los numeros")
         left.pack(fill=BOTH)
@@ -38,16 +48,15 @@ class mainApp(Tk):
         Tk.__init__(self)
         self.title("Calendario Universal")
         self.geometry("532x422")
-        #self.configure(bg="#A9D0F5")
+        # self.configure(bg="#A9D0F5")
 
         __btnLastYear = calendarButton(self, text="<<").place(x=24, y=5)
         __btnLastMonth = calendarButton(self, text="<").place(x=83, y=5)
         __btnNextMonth = calendarButton(self, text=">").place(x=398, y=5)
         __btnNextYear = calendarButton(self, text=">>").place(x=457, y=5)
 
-        mesDisplay(self).place(x=0,y=0)
-
-
+        self.__display = mesDisplay(self)
+        self.__display.place(x=202, y=5)
 
     def start(self):
         self.mainloop()
